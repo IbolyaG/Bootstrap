f96de3bc2598c85d875396f4adb23b962be5131f
IbolyaG <127603033+IbolyaG@users.noreply.github.com>
2023-07-22 14:02:26 +0200

    Add files via upload

---
+++ bootstrapVizsga/bootstrapV.js
@@ -0,0 +1,12 @@
+/*function Click1(){
+   if(Click1){
+    document.getElementsByClassName("btnfor404").innerHTML= window.open("boostrap404.html");
+        }
+    };
+
+   
+    function Click1(){
+        
+    $(".btnfor404"). load("bootstrap404.html");
+}; */
+        
